c5d173e70b831a1963e142fcb0ff647e3b567fc0
silviadebussi <silviadebussi2@gmail.com>
2023-03-23 23:04:28 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from random import *
+
+jogada = int(input('Insira sua jogada: '))
+
+maquina = randint(0,2)
+
+print(maquina)
+
+if jogada == 0 and maquina == 1:
+    print('Maquina venceu')
+else:
+    if jogada == 0 and maquina == 2:
+        print('Jogador venceu')
+    else:
+        if jogada == 0 and maquina == 0:
+            print('Empate')
+        else:
+            if jogada == 1 and maquina == 1:
+                print('Empate')
+            else:
+                if jogada == 1 and maquina == 0:
+                    print('jogador Venceu')
+                else:
+                    if jogada == 1 and maquina == 2:
+                     print('Máquina venceu')
+                    else:
+                        if jogada == 2 and maquina == 0:
+                            print('Máquina venceu')
+                        else:
+                            if jogada == 2 and maquina == 1:
+                                print('Jogador venceu')
+                            else:
+                                if jogada == 2 and maquina == 2:
+                                    print('Empate')
+                                else:
+                                    print('Inválido')
